b6d3d3332f83b129b0151639e6c6a656924cee88
Almir Rogério Camolesi <camolesi@fema.edu.br>
2025-09-06 14:39:06 -0300

    Criação de Página Advogados Manualmente: Listar (index) e Inserir (create)

---
+++ Controllers/AdvController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SisJur.Models;
+
+namespace SisJur.Controllers
+{
+    public class AdvController : Controller
+    {
+        private readonly Contexto _context;
+
+        public AdvController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: Adv
+        public async Task<IActionResult> Index()
+        {
+            var contexto = _context.Advogados.Include(a => a.area);
+            return View(await contexto.ToListAsync());
+        }
+
+        //get: Adv/Create
+        [HttpGet]
+        public IActionResult Create ()
+        {
+            ViewData["areaid"] = new SelectList(_context.Areas, "id", "descricao");
+            return View();
+        }
+
+        //post: Adv/Create
+        [HttpPost] 
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,nome,cidade,estado,idade,areaid")] Advogado advogado)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(advogado);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["areaid"] = new SelectList(_context.Areas, "id", "descricao", advogado.areaid);
+            return View(advogado);
+        }
+
+
+
+    }
+}
--- Models/Advogado.cs
+++ Models/Advogado.cs
@@ -13,29 +13,30 @@ namespace SisJur.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id { get; set; }
 
-        [Display(Name = "Nome")]
+        [Display(Name = "NOME")]
         [Column("nome", TypeName = "varchar(35)")]
         [StringLength(35, ErrorMessage = "Campo excede 35 caracteres")]
         [Required(ErrorMessage = "O campo Nome é obrigatório.")]
         public string nome { get; set; }
 
-        [Display(Name = "Cidade")]
+        [Display(Name = "CIDADE")]
         [StringLength(25, MinimumLength =5 , ErrorMessage ="tamanho entre 5 e 25 caracteres")]
         [Required(ErrorMessage = "O campo Cidade é obrigatório.")]
         public string cidade { get; set; }
 
-        [Display(Name = "Estado")]
+        [Display(Name = "ESTADO")]
         [StringLength(2)]
         [Required(ErrorMessage = "O campo Estado é obrigatório.")]
         public string estado { get; set; }
         
-        [Display(Name = "Idade")]
+        [Display(Name = "IDADE")]
         [Range(21, 110, ErrorMessage = "A idade deve estar entre 18 e 120 anos.")]
         public int idade { get; set; }
 
-        [Display(Name = "Área")]
+       
         public int areaid { get; set; }
 
+        [Display(Name = "ÁREA")]
         [ForeignKey("areaid")]
         public virtual Area area { get; set; }
 
